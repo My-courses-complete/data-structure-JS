cc66dfbb55b913dd725cfe020de3a8f0059281b3
JMR-Mejia <chucho4720013@gmail.com>
2021-04-27 20:51:07 -0500

    Construyendo una Singly Linked List

---
+++ linkedList/singly.js
@@ -0,0 +1,43 @@
+1-- > 2-- > 3-- > 4-- > 5-- > null;
+
+let singlyLinkedList = {
+  head: {
+    value: 1,
+    next: {
+      value: 2,
+      next: {
+        value: 3,
+        next: {
+          value: 4,
+          next: {
+            value: 5,
+            next: {
+              value: null,
+            },
+          },
+        },
+      },
+    },
+  },
+};
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class MySinglyListkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+    };
+
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
+
+let myLinkedList = new MySinglyListkedList(1);
